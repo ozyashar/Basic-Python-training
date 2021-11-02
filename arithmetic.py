ffe7bd84914981f1640df8cb10fa369bae3174b8
ozyashar <93587165+ozyashar@users.noreply.github.com>
2021-11-02 15:12:57 +0200

    Delete arithmetic.py

--- arithmetic.py
+++
@@ -1,6 +0,0 @@
-# simple arithmetic operations
-x = input('please enter variable: ')
-y = input('please enter variable: ')
-print('x+y=', int(x) + int(y)) # todo: operation should be user input also
-print('x/y=', int(x)//int(y))
-print('x//y=', int(x)/int(y))
