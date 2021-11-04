b86cbd006e27803fd676bb790cca2873a207b523
ozyashar <93587165+ozyashar@users.noreply.github.com>
2021-11-04 16:26:40 +0200

    Update arithmetic.py

--- Ex1/arithmetic.py
+++ Ex1/arithmetic.py
@@ -1,6 +1,26 @@
 # simple arithmetic operations
-x = input('please enter variable: ')
-y = input('please enter variable: ')
-print('x+y=', int(x) + int(y)) # todo: operation should be user input also
-print('x/y=', int(x)//int(y))
-print('x//y=', int(x)/int(y))
+x = input('Please insert x variable: ')
+y = input('Please insert y variable: ')
+x = int(x)
+y = int(y)
+operation = input('Please insert math operation: ')
+
+if operation == "+":
+    calc = x + y
+    print(calc)
+elif operation == "-":
+    calc = x - y
+    print(calc)
+elif operation == "*":
+    calc = x * y
+    print(calc)
+elif operation == "/":
+    if y == 0:
+        print("Div by Zero")
+    calc = x * y
+    print(calc)
+elif operation == "//":
+    calc = x // y
+    print(calc)
+else:
+    print("operation is not valid")
