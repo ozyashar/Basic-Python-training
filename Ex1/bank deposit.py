31367718f169179b57cb0d9c94a2b861ae34e288
ozyashar <93587165+ozyashar@users.noreply.github.com>
2021-11-04 15:52:17 +0200

    Update bank deposit.py

--- Ex1/bank deposit.py
+++ Ex1/bank deposit.py
@@ -3,16 +3,19 @@
 dValue = input('Please enter deposit value: ')
 # percent per year - 3%
 Percent = input('Please enter percent per year: ')
-# number of years - 3
-# This counter will stop after reaching 3 years.
+Years = input('Please enter number of Years: ')
+
+dValue = int(dValue)
+Percent = int(Percent)
+Years = int(Years)
+
 # todo: number of years should be user input
 count = 0
-while int(dValue) > 0: # todo: no need to convert variable dValue to int every time
-    dValue = int(dValue) * (1 + (int(Percent)/10))
+while dValue > 0:  # todo: no need to convert variable dValue to int every time
+    dValue = dValue * (1 + (Percent / 10))
     count += 1
-    if count == 3:
-        print("Result: ", int(dValue))
+    if count == Years:
+        print("Result: ", dValue)
         break
-
 else:
-    print('Reached to end', dValue) # todo: statement is unreachable
+    print('Please enter Deposit again with (+)', dValue)  # todo: statement is unreachable ()
