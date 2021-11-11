1287023b0f7c93f5f29c8dcaf018449e04d2caff
Vitalii Onyshchuk <vitaliy.onyschuk-ext@jci.com>
2021-11-11 14:33:11 +0200

    reviewed

--- Ex2.py
+++ Ex2.py
@@ -5,7 +5,7 @@ i = 0
 EvenList = []
 OddList = []
 
-while int(i) < 8:
+while int(i) < 8: # todo: why 8? should be a list of arbitrary length
     N = input("Enter a number: ")
 
     if int(N) % 2 == 0:
@@ -14,6 +14,6 @@ while int(i) < 8:
         OddList.append(N)
 
     i += 1
-
+# todo: another task. try to divide this task into two part. 1. input list, 2. divide into odd and even
 print("EvenList: ", EvenList)
 print("OddList: ", OddList)
