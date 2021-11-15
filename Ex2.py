f73f550a639b9496020c1c9eecf02f02059a5bc2
ozyashar <93587165+ozyashar@users.noreply.github.com>
2021-11-15 08:38:28 +0200

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -5,13 +5,9 @@ i = 0
 EvenList = []
 OddList = []
 
-<<<<<<< HEAD
+
 while int(i) < 8:
     lst = input("Enter a number: ")
-=======
-while int(i) < 8: # todo: why 8? should be a list of arbitrary length
-    N = input("Enter a number: ")
->>>>>>> 1287023b0f7c93f5f29c8dcaf018449e04d2caff
 
     if int(lst) % 2 == 0:
         EvenList.append(lst)
