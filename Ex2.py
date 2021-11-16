9a20c3916b0a99d0afc60410e98540dfb76c03e0
ozyashar <93587165+ozyashar@users.noreply.github.com>
2021-11-16 15:12:18 +0200

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -1,22 +1,28 @@
-# divide list into two: even and odd numbers
-
+# divide list into two: even and odd numbers + Sum + Min + Max
 i = 0
-
 EvenList = []
 OddList = []
-
-
+sSum = int()
+lMax = int()
+lMin = int()
 while int(i) < 8:
     lst = input("Enter a number: ")
-
     if int(lst) % 2 == 0:
         EvenList.append(lst)
     else:
         OddList.append(lst)
-
     i += 1
+    sSum = sSum + int(lst)
+    if int(lMax) < int(lst):
+        lMax = lst
+    if i == 0:
+        lMin = lst
+    else:
+        if int(lMin) > int(lst):
+            lMin = lst
+
 # todo: another task. try to divide this task into two part. 1. input list, 2. divide into odd and even
 print("EvenList: ", EvenList)
 print("OddList: ", OddList)
-
-
+print("Sum: ", sSum)
+print("Min:", lMin, "Max:", lMax)
